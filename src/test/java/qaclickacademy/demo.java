1df193adc6be5d94ecf711875d407fa867d4cdc0
Raghavendra Anand Gotur <Raghavendra.gotur@bridgesgi.com>
2021-11-15 14:29:06 +0530

    new file created

---
+++ src/test/java/qaclickacademy/demo.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void newTest() {
+		System.out.println("I am New Test to master branch.");
+	}
+}
